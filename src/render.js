002a3c8c4b9eb3c5ea9fbef9638989d9afef7c1a
Dan Lehman <5494894+vegaZenith@users.noreply.github.com>
2026-09-24 11:40:35 -0400

    refactor form rendering

--- src/render.js
+++ src/render.js
@@ -13,64 +13,112 @@ let createSelectOption = function (value, content) {
     return option;
 }
 
+let renderAddTodoForm = function () {
+    let todoForm = document.createElement("form");
+    todoForm.setAttribute("action", "index.html");
+    todoForm.setAttribute("method", "post")
+    let titleInput = createFormInput("text");
+    let descriptionInput = createFormInput("text");
+    let dueDateInput = createFormInput("date");
+    let priorityInput = document.createElement("select");
+    priorityInput.setAttribute("name", "priority");
+    priorityInput.appendChild(createSelectOption("Low", "Low"));
+    priorityInput.appendChild(createSelectOption("Medium", "Medium"));
+    priorityInput.appendChild(createSelectOption("High", "High"));
+
+    let submitButton = document.createElement("button");
+
+    submitButton.innerText = "Submit";
+    submitButton.setAttribute("type", "button");
+    submitButton.addEventListener("click", () => {
+        // create new todo list
+        let newTodo = makeTodo();
+        newTodo.title = titleInput.value;
+        newTodo.description = descriptionInput.value;
+        newTodo.dueDate = dueDateInput.value;
+        newTodo.priority = priorityInput.value;
+        addTodo(newTodo);
+        renderMain();
+    });
+    todoForm.appendChild(titleInput);
+    todoForm.appendChild(descriptionInput);
+    todoForm.appendChild(dueDateInput);
+    todoForm.appendChild(priorityInput);
+    todoForm.appendChild(submitButton);
+    return todoForm
+}
+
 let renderMain = function () {
+    //clear main content element
     let body = document.querySelector("#main");
     body.innerHTML = "";
-    let todoList = document.createElement("div");
-    todoList.setAttribute("id", "todo-list");
+    //create todo list
+    let todoList = renderAllTodos();
     let addButton = document.createElement("button");
     addButton.innerText = "Add";
 
     body.appendChild(todoList);
-    renderAllTodos();
     body.appendChild(addButton);
+
+    // add new todo list
     addButton.addEventListener("click", () => {
+        // create new todo list form
         addButton.disabled = true;
-        let todoForm = document.createElement("form");
-        todoForm.setAttribute("action", "index.html");
-        todoForm.setAttribute("method", "post")
-        let titleInput = createFormInput("text");
-        let descriptionInput = createFormInput("text");
-        let dueDateInput = createFormInput("date");
-        let priorityInput = document.createElement("select");
-        priorityInput.setAttribute("name", "priority");
-        priorityInput.appendChild(createSelectOption("Low", "Low"));
-        priorityInput.appendChild(createSelectOption("Medium", "Medium"));
-        priorityInput.appendChild(createSelectOption("High", "High"));
 
-        let submitButton = document.createElement("button");
-
-        submitButton.innerText = "Submit";
-        submitButton.setAttribute("type", "button");
-        submitButton.addEventListener("click", () => {
-            let newTodo = makeTodo();
-            newTodo.title = titleInput.value;
-            newTodo.description = descriptionInput.value;
-            newTodo.dueDate = dueDateInput.value;
-            newTodo.priority = priorityInput.value;
-            addTodo(newTodo);
-            renderMain();
-        });
-        todoForm.appendChild(titleInput);
-        todoForm.appendChild(descriptionInput);
-        todoForm.appendChild(dueDateInput);
-        todoForm.appendChild(priorityInput);
-        todoForm.appendChild(submitButton);
-        body.appendChild(todoForm);
+        body.appendChild(renderAddTodoForm());
     });
 };
 
 
 let renderAllTodos = function () {
+    let todoList = document.createElement("div");
+    todoList.setAttribute("id", "todo-list");
     let todos = getTodos();
-    let todoList = document.querySelector("#todo-list");
     todos.forEach(todo => {
         let todoElement = renderTodo(todo);
         todoList.appendChild(todoElement);
     });
+    return todoList;
+}
+
+let renderEditTodoForm = function (todo) {
+    let editForm = document.createElement("form");
+    editForm.setAttribute("action", "index.html");
+    editForm.setAttribute("method", "post")
+    let titleInput = createFormInput("text");
+    titleInput.value = todo.title;
+    let descriptionInput = createFormInput("text");
+    descriptionInput.value = todo.description;
+    let dueDateInput = createFormInput("date");
+    dueDateInput.value = todo.dueDate;
+    let priorityInput = document.createElement("select");
+
+    priorityInput.setAttribute("name", "priority");
+    priorityInput.appendChild(createSelectOption("Low", "Low"));
+    priorityInput.appendChild(createSelectOption("Medium", "Medium"));
+    priorityInput.appendChild(createSelectOption("High", "High"));
+    priorityInput.value = todo.priority;
+
+    let confirmButton = document.createElement("button");
+    confirmButton.innerText = "Confirm";
+    confirmButton.addEventListener("click", () => {
+        todo.title = titleInput.value;
+        todo.description = descriptionInput.value;
+        todo.dueDate = dueDateInput.value;
+        todo.priority = priorityInput.value;
+        renderMain();
+    });
+
+    editForm.appendChild(titleInput);
+    editForm.appendChild(descriptionInput);
+    editForm.appendChild(dueDateInput);
+    editForm.appendChild(priorityInput);
+    editForm.appendChild(confirmButton);
+    return editForm;
 }
 
 let renderTodo = function (todo) {
+    // render individual elements
     let todoBody = document.createElement("div");
     let title = document.createElement("h2");
     title.textContent = todo.title;
@@ -87,42 +135,11 @@ let renderTodo = function (todo) {
     }
     let notes = document.createElement("p");
     notes = todo.notes;
+
     let editButton = document.createElement("button");
     editButton.innerText = "Edit";
     editButton.addEventListener("click", () => {
-        let editForm = document.createElement("form");
-        editForm.setAttribute("action", "index.html");
-        editForm.setAttribute("method", "post")
-        let titleInput = createFormInput("text");
-        titleInput.value = todo.title;
-        let descriptionInput = createFormInput("text");
-        descriptionInput.value = todo.description;
-        let dueDateInput = createFormInput("date");
-        dueDateInput.value = todo.dueDate;
-        let priorityInput = document.createElement("select");
-   
-        priorityInput.setAttribute("name", "priority");
-        priorityInput.appendChild(createSelectOption("Low", "Low"));
-        priorityInput.appendChild(createSelectOption("Medium", "Medium"));
-        priorityInput.appendChild(createSelectOption("High", "High"));
-        priorityInput.value = todo.priority;
-
-        let confirmButton = document.createElement("button");
-        confirmButton.innerText = "Confirm";
-        confirmButton.addEventListener("click", () => {
-           todo.title = titleInput.value;
-            todo.description = descriptionInput.value;
-            todo.dueDate = dueDateInput.value;
-            todo.priority = priorityInput.value;
-            renderMain();
-        });
-
-        editForm.appendChild(titleInput);
-        editForm.appendChild(descriptionInput);
-        editForm.appendChild(dueDateInput);
-        editForm.appendChild(priorityInput);
-        editForm.appendChild(confirmButton);
-        todoBody.appendChild(editForm);
+        todoBody.appendChild(renderEditTodoForm(todo));
     });
 
     todoBody.appendChild(title);
